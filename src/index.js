dfdced7207a245f03f156e9dabfde4ed092e48c2
Fedox-die-Ente <mrminecrafttv19@gmail.com>
2023-11-19 14:54:59 +0000

    8 - Command syntax validations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-const CommandHandler = require("./CommandHandler");
+const mongoose = require("mongoose");
+
+const CommandHandler = require("./command-handler/CommandHandler");
 
 class FedCommands {
-    constructor({ client, commandsDir }) {
+    constructor({ client, mongoUri, commandsDir }) {
         if(!client) {
             throw new Error('[FedCommands] Client is required!')
+        }
+
+        if (mongoUri) {
+            this.connectToMongo(mongoUri)
         }
 
         if (commandsDir) {
             new CommandHandler(commandsDir, client)
         }
+    }
+
+    connectToMongo(mongoUri) {
+        mongoose.set('strictQuery', false);
+        mongoose.connect(mongoUri, {
+            keepAlive: true
+        })
     }
 }
 
